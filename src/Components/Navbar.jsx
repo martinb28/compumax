6d0d837d608157ba8c9e0389472a65abc3435ec7
martinb28 <bugao1991@gmail.com>
2021-10-09 14:07:46 -0300

    apply del navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Navbar = () => {
+    return (
+        <header>
+            <nav>
+                <img src="../../../logo192.png" alt="logo"/>
+                <h1 className = "item-nav">Home</h1>
+                <h1 className = "item-nav">Profile</h1>
+                <h1 className = "item-nav">Sign In</h1>
+            </nav>
+        </header>
+    )
+}

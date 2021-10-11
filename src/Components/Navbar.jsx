aae4eb8989a9bfccc3c0238683c047897156debc
martinb28 <bugao1991@gmail.com>
2021-10-10 21:11:49 -0300

    CompuMaxBugao

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 
 export const Navbar = () => {
     return (
-        <header>
-            <nav>
-                <img src="../../../logo192.png" alt="logo"/>
-                <h1 className = "item-nav">Home</h1>
-                <h1 className = "item-nav">Profile</h1>
-                <h1 className = "item-nav">Sign In</h1>
+        <header className="header">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Compumax_logo.svg/1200px-Compumax_logo.svg.png" alt="logo" className= "logo"/>
+            <nav className="navbar">                
+                <p className = "item-nav">Notebooks</p>
+                <p className = "item-nav">Pc Armadas</p>
+                <p className = "item-nav">Hardware</p>
+                <p className = "item-nav">Perifericos</p>
+                <p className = "item-nav">Contactos</p>
             </nav>
         </header>
-    )
-}
+    );
+};
+
+export default Navbar;
+

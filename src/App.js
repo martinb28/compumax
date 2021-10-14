87a774dee65b07afac645299e440fe8be8f3d26e
martinb28 <bugao1991@gmail.com>
2021-10-14 20:52:16 -0300

    'CompuMaxBugao'

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Navbar } from './Components/Navbar';
-import { ItemListContainer } from './Container/ItemListContainer';
+import { ItemListContainer } from './Components/Container/ItemListContainer';
 
 function App() {
   return (

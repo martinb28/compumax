30c1ebee42ff9c27eff89666f334d430e8ed1769
martinb28 <bugao1991@gmail.com>
2021-10-18 23:24:21 -0300

    CompuMaxBugao

--- src/Components/Container/ItemList/Item/Item.js
+++ src/Components/Container/ItemList/Item/Item.js
@@ -1,14 +1,17 @@
 import React from 'react'
 
 const Item = ({nombre, desc, precio, imagen}) => {
-    return (
-        <div>
-            <h2>{nombre}</h2>
-            <h2>{desc}</h2>
-            <h2>${precio}</h2>            
-            <img src={imagen} alt = {nombre}/>
-
+    return(
+    <div class="max-w-md flex rounded flex-wrap overflow-hidden shadow-sm">
+        <img class="w-40 h-40"src={imagen} alt = {nombre}/>
+        <div class="px-6 py-4">
+            <div class="font-bold text-xl mb-2">{nombre}</div>
+                <p class="text-gray-700 text-base">{desc}</p>
+            </div>
+        <div class="px-6 pt-4 pb-2">
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">${precio}</span>    
         </div>
+    </div>
     )
 }
 
--- src/Data/Data.js
+++ src/Data/Data.js
@@ -1,48 +1,48 @@
 const Data = [
     {
         id:'1',
-        nombre:'Gabinete 1',
-        desc: 'descripcion del item',
-        precio: '10000',
+        nombre:'GABINETE AEROCOOL ZAURON FRGB',
+        desc: 'Gabinete mid tower de alto rendimiento con un diseño de panel frontal audaz y elegante. ',
+        precio: '4450',
         categoria: 'gabinetes',
         stock: '5',
-        imagen:'/img/gabinete.jpg'
+        imagen:'/img/Gabinete.png'
     },
     {
         id:'2',
-        nombre:'Gabinete 2',
-        desc: 'descripcion del item',
-        precio: '10000',
-        categoria: 'gabinetes',
+        nombre:'PC armada I7',
+        desc: 'El Core i7 de 9ª Generación es capaz de brindar el mejor rendimiento en tareas como producción musical, edición y programación. ',
+        precio: '79990',
+        categoria: 'PC armadas',
         stock: '5',
-        imagen:'/img/gabinete.jpg'
+        imagen:'/img/PCArmada.jpg'
     },
     {
         id:'3',
-        nombre:'Gabinete 3',
-        desc: 'descripcion del item',
-        precio: '10000',
-        categoria: 'gabinetes',
+        nombre:'Notebook Asus',
+        desc: 'Ya sea para trabajar o jugar, el ASUS X515 es el computador portátil que ofrece un potente rendimiento.',
+        precio: '71999',
+        categoria: 'Notebooks',
         stock: '5',
-        imagen:'/img/gabinete.jpg'
+        imagen:'/img/Notebook.jpg'
     },
     {
         id:'4',
-        nombre:'Gabinete 4',
-        desc: 'descripcion del item',
-        precio: '10000',
-        categoria: 'gabinetes',
+        nombre:'Placa de video Palit NVIDIA Geforce RTX 3090 GAMING PRO 24GB',
+        desc: 'La GeForce RTX ™ 3090 es enormemente poderosa en todos los sentidos.',
+        precio: '523999',
+        categoria: 'Hardware',
         stock: '5',
-        imagen:'/img/gabinete.jpg'
+        imagen:'/img/PlacaDeVideo.jpg'
     },
     {
         id:'5',
-        nombre:'Gabinete 5',
-        desc: 'descripcion del item',
-        precio: '10000',
-        categoria: 'gabinetes',
+        nombre:'Auricular inalambrico Logitech G G733 Black RGB',
+        desc: 'Por fin, unos audífonos que pueden ser tan expresivos como tú.',
+        precio: '15690',
+        categoria: 'Perifericos',
         stock: '5',
-        imagen:'/img/gabinete.jpg'
+        imagen:'/img/auris.jpg'
     },
 ]
 

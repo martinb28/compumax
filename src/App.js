77646ccf3095a43b077f83d178dc4926b67f7a8a
martinb28 <bugao1991@gmail.com>
2021-10-31 19:37:14 -0300

    CompuMaxBugao

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import './index.css';
 import NavBar from './Components/NavBar';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import ItemListContainer from './Components/Container/ItemListContainer';
 
 function App() {
     return (
         <>
-            <NavBar/>
-            <ItemListContainer/>
+            <BrowserRouter>
+                <NavBar/>
+                <Switch>
+                    <Route exact path = '/'>
+                        <ItemListContainer/>
+                    </Route>
+                    <Route exact path = '/Notebooks'>
+                        <h1>Notebooks</h1>
+                    </Route>
+                    <Route exact path = '/Hardware'>
+                        <ItemListContainer/>
+                    </Route>
+                    <Route exact path = '/perifericos'>
+                        <ItemListContainer/>
+                    </Route>
+                </Switch>
+            </BrowserRouter>    
         </>
     );
 }

c2f649ff1ee4651def05cee848d63c991d0626e9
martinb28 <bugao1991@gmail.com>
2021-11-16 22:07:45 -0300

    edit

--- src/lib/firebaseConfig.js
+++ src/lib/firebaseConfig.js
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyBWMWfzAdRCK7uwgJdZOZSL2W56ocqyYw8',
-  authDomain: 'process.env.REACT_APP_AUTH_DOMAIN',
-  projectId: 'compumaxbugao',
-  storageBucket: 'compumaxbugao.appspot.com',
-  messagingSenderId: '329751266865',
-  appId: '1:329751266865:web:7d059d098d4698afe0f710'
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID
 };
 
 // Initialize Firebase

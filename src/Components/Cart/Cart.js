6749491ef51343ae327f69b314e9a495643aca1d
martinb28 <bugao1991@gmail.com>
2021-11-18 18:46:15 -0300

    CompuMaxBugao

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -10,9 +10,9 @@ const Cart = (props) =>{
     {cart.map((item)=>
             <><div className="flex flex-col border bg-gray-50">
             <h2 className='font-semibold'>nombre:</h2> <h3 className='text-blue-500'>{item.nombre}</h3>
-            <h2 className='font-semibold'>precio:</h2> <h3 className='text-blue-500'>{item.precio}</h3>
+            <h2 className='font-semibold'>precio:</h2> <h3 className='text-blue-500'>${item.precio}</h3>
             <h2 className='font-semibold'>cantidad:</h2> <h3 className='text-blue-500'>{item.cantidad}</h3>
-            <h2 className='font-semibold'>subtotal:</h2> <h3 className='text-blue-500'>{item.subtotal}</h3>
+            <h2 className='font-semibold'>subtotal:</h2> <h3 className='text-blue-500'>${item.subtotal}</h3>
             <button onClick={() => quitarItem(item.id)} className='content-start font-semibold text-red-600'>Quitar objeto</button>
 
         </div><div><button className='content-start' onClick={() => vaciarCarro()}> Vaciar carro</button></div></>

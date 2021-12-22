11a2195a417c014d3fce3225d5cc44df692790c2
martinb28 <bugao1991@gmail.com>
2021-12-21 22:03:45 -0300

    CompuMaxBugao

--- src/Components/FormBuy/FormBuy.js
+++ src/Components/FormBuy/FormBuy.js
@@ -33,14 +33,16 @@ export default function FormBuy() {
     }
 
     return (
-        <div>
-            <label className='font-semibold'>Nombre</label><br/>
-            <input style={{'border':"1px solid black"}} type='text' onChange={(e)=>setNombre(e.target.value)} /><br/>
-            <label className='font-semibold'>E-mail</label><br/>
-            <input style={{'border':"1px solid black"}} type='email' onChange={(e)=>setMail(e.target.value)} /><br/>
-            <label className='font-semibold'>Telefono</label><br/>
-            <input style={{'border':"1px solid black"}} type='tel' onChange={(e)=>setTelefono(e.target.value)} /><br/><br/>
-            <button onClick={addPedido} className= "bg-green-300 hover:bg-green-400 text-white font-bold py-2 px-4 border border-green-400 rounded">Finalizar compra</button>
+        <div className = 'mx-96 bg-gray-700 '>
+            <div className = 'ml-72 mt-48'>
+                <label className='font-semibold text-white'>Nombre</label><br/>
+                <input style={{'border':"1px solid black"}} type='text' onChange={(e)=>setNombre(e.target.value)} /><br/>
+                <label className='font-semibold text-white'>E-mail</label><br/>
+                <input style={{'border':"1px solid black"}} type='email' onChange={(e)=>setMail(e.target.value)} /><br/>
+                <label className='font-semibold text-white'>Telefono</label><br/>
+                <input style={{'border':"1px solid black"}} type='tel' onChange={(e)=>setTelefono(e.target.value)} /><br/><br/>
+                <button onClick={addPedido} className= "bg-green-300 hover:bg-green-400 text-white font-bold py-2 px-4 border border-green-400 rounded">Finalizar compra</button>
+            </div>
         </div>
     )
 }
